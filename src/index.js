e5f63287d24585bc50771dba5a03975ab58a0821
rishav21556 <social.rishav.2003@gmail.com>
2024-10-20 03:53:48 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function App(){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path="/DevLog">
           <Route index element={<RepoIndex />} />
         </Route>
       </Routes>
